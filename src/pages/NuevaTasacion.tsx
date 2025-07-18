2204c3081f1065c3f4f65053668b3d3f94b8e4ac
Svalent99 <valen.sotelo.123@gmail.com>
2025-07-18 13:02:44 -0300

    df

--- src/pages/NuevaTasacion.tsx
+++ src/pages/NuevaTasacion.tsx
@@ -179,7 +179,7 @@ const NuevaTasacion = () => {
 
   const handleSuccessModalClose = () => {
     setShowSuccessModal(false);
-    navigate('/dashboard');
+    navigate('/dashboard/tasaciones');
   };
 
   if (loading) {
